49de4bd0d87b0cac350edc20b758e88a897e5c2b
Philip (eselmeister) Wenig <philip.wenig@gmx.net>
2010-10-14 13:46:21 +0200

    Initial git import.

---
+++ openchrom/plugins/net.openchrom.thirdpartylibraries.netcdf/src/net/openchrom/thirdpartylibraries/netcdf/Activator.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Philip (eselmeister) Wenig. Copyright 1997-2004
+ * Unidata Program Center/University Corporation for Atmospheric Research, P.O.
+ * Box 3000, Boulder, CO 80307, support@unidata.ucar.edu.
+ * 
+ * This library is free
+ * software; you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details. You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston MA 02111-1307, USA
+ *******************************************************************************/
+package net.openchrom.thirdpartylibraries.netcdf;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "net.openchrom.thirdpartylibraries.netcdf";
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+
+		super.start(context);
+		plugin = this;
+		initLogger();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+
+		return plugin;
+	}
+
+	/**
+	 * Reading the log4j properties.
+	 */
+	private void initLogger() {
+
+		PropertyConfigurator.configure(getBundle().getEntry("/log4j.properties"));
+	}
+}
