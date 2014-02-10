f97189d1b7cc690fba2900d07fb696f8788dfa12
Dr. Philip Wenig <philip.wenig@openchrom.net>
2014-02-10 09:01:36 +0100

    I've edited the build.properties. This hopefully resolves the bundle starting issue.

--- openchrom/plugins/net.openchrom.thirdpartylibraries.netcdf/src/net/openchrom/thirdpartylibraries/netcdf/Activator.java
+++ openchrom/plugins/net.openchrom.thirdpartylibraries.netcdf/src/net/openchrom/thirdpartylibraries/netcdf/Activator.java
@@ -33,33 +33,55 @@
  *******************************************************************************/
 package net.openchrom.thirdpartylibraries.netcdf;
 
-import org.osgi.framework.BundleActivator;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
 
-	private static BundleContext context;
+	// The plug-in ID
+	public static final String PLUGIN_ID = "net.openchrom.thirdpartylibraries.netcdf";
+	// The shared instance
+	private static Activator plugin;
 
-	static BundleContext getContext() {
+	/**
+	 * The constructor
+	 */
+	public Activator() {
 
-		return context;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
+	public void start(BundleContext context) throws Exception {
 
-		Activator.context = bundleContext;
+		super.start(context);
+		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext bundleContext) throws Exception {
+	public void stop(BundleContext context) throws Exception {
 
-		Activator.context = null;
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+
+		return plugin;
 	}
 }
