de2096e85281aa07a9a9f635f26ddaf867408e32
Dr. Philip Wenig <philip.wenig@openchrom.net>
2014-10-01 17:52:50 +0200

    The javabase64 library has been removed. The dom4j library is fetched from the Eclipse Orbit repo now.

--- chemclipse/plugins/net.chemclipse.thirdpartylibraries.supercsv/src/net/openchrom/thirdpartylibraries/supercsv/Activator.java
+++ chemclipse/plugins/net.chemclipse.thirdpartylibraries.supercsv/src/net/openchrom/thirdpartylibraries/supercsv/Activator.java
@@ -13,7 +13,7 @@
  * KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  *******************************************************************************/
-package net.chemclipse.thirdpartylibraries.supercsv;
+package net.openchrom.thirdpartylibraries.supercsv;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
