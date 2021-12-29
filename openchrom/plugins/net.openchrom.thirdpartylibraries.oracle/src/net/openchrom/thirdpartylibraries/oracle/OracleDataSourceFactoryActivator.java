a68fe8e154366127595e4464d3fd8296c2a8df1d
Philip Wenig <philip.wenig@openchrom.net>
2021-12-29 08:04:45 +0100

    Warnings have been reduced.

--- openchrom/plugins/net.openchrom.thirdpartylibraries.oracle/src/net/openchrom/thirdpartylibraries/oracle/OracleDataSourceFactoryActivator.java
+++ openchrom/plugins/net.openchrom.thirdpartylibraries.oracle/src/net/openchrom/thirdpartylibraries/oracle/OracleDataSourceFactoryActivator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Lablicate GmbH.
+ * Copyright (c) 2020, 2021 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -8,6 +8,7 @@
  * 
  * Contributors:
  * Christoph Läubrich - initial api and implementation
+ * Philip Wenig - refactoring logger
  *******************************************************************************/
 package net.openchrom.thirdpartylibraries.oracle;
 
@@ -26,11 +27,15 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.jdbc.DataSourceFactory;
 import org.osgi.service.log.LogService;
+import org.osgi.service.log.Logger;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class OracleDataSourceFactoryActivator implements BundleActivator {
 
 	private static final String ORACLE_JDBC_LIB_PATH = "ORACLE_JDBC_LIB_PATH";
+	private static final int INFO = 0;
+	private static final int WARNING = 1;
+	private static final int ERROR = 2;
 	private OracleDataSourceFactory oracleDataSourceFactory;
 
 	/*
@@ -43,7 +48,7 @@ public class OracleDataSourceFactoryActivator implements BundleActivator {
 		ServiceTracker<LogService, LogService> tracker = new ServiceTracker<>(bundleContext, LogService.class, null);
 		try {
 			File dataFile = getLibPath(bundleContext);
-			log(tracker, LogService.LOG_INFO, "Searching for Oracle JDBC libraries @ " + dataFile.getAbsolutePath());
+			log(tracker, INFO, "Searching for Oracle JDBC libraries @ " + dataFile.getAbsolutePath());
 			List<URL> urls = new ArrayList<>();
 			if(dataFile.isDirectory()) {
 				File[] listFiles = dataFile.listFiles();
@@ -56,7 +61,7 @@ public class OracleDataSourceFactoryActivator implements BundleActivator {
 				}
 			}
 			if(urls.isEmpty()) {
-				log(tracker, LogService.LOG_WARNING, "No library files found, OracleDataSourceFactory service will not be avaiable");
+				log(tracker, WARNING, "No library files found, OracleDataSourceFactory service will not be avaiable");
 				return;
 			}
 			oracleDataSourceFactory = new OracleDataSourceFactory(new URLClassLoader(urls.toArray(new URL[0]), getClass().getClassLoader()));
@@ -66,10 +71,10 @@ public class OracleDataSourceFactoryActivator implements BundleActivator {
 				properties.put(DataSourceFactory.OSGI_JDBC_DRIVER_CLASS, driver.getClass().getName());
 				properties.put(DataSourceFactory.OSGI_JDBC_DRIVER_NAME, driver.getClass().getSimpleName());
 				properties.put(DataSourceFactory.OSGI_JDBC_DRIVER_VERSION, driver.getMajorVersion() + "." + driver.getMinorVersion());
-				log(tracker, LogService.LOG_INFO, "Register OracleDataSourceFactory service with properties " + properties);
+				log(tracker, INFO, "Register OracleDataSourceFactory service with properties " + properties);
 				bundleContext.registerService(DataSourceFactory.class, oracleDataSourceFactory, properties);
 			} catch(SQLException e) {
-				log(tracker, LogService.LOG_ERROR, "Can't create driver, OracleDataSourceFactory service will not be avaiable (" + e.getCause() + ")");
+				log(tracker, ERROR, "Can't create driver, OracleDataSourceFactory service will not be avaiable (" + e.getCause() + ")");
 			}
 		} finally {
 			tracker.close();
@@ -93,7 +98,20 @@ public class OracleDataSourceFactoryActivator implements BundleActivator {
 
 		LogService logService = tracker.getService();
 		if(logService != null) {
-			logService.log(level, message);
+			Logger logger = logService.getLogger(OracleDataSourceFactoryActivator.class);
+			if(logger != null) {
+			}
+			switch(level) {
+				case WARNING:
+					logger.warn(message);
+					break;
+				case ERROR:
+					logger.error(message);
+					break;
+				default:
+					logger.info(message);
+					break;
+			}
 		} else {
 			System.out.println("[" + getClass().getSimpleName() + "] " + message);
 		}
