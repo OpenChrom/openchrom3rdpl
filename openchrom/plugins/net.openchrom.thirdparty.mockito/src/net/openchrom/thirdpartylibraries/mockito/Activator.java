42bc501ae281e1b0fad0594b3c3ce493b682d9b2
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-03-21 19:44:00 +0100

    thirdpartylibraries → thirdparty

---
+++ openchrom/plugins/net.openchrom.thirdparty.mockito/src/net/openchrom/thirdpartylibraries/mockito/Activator.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2018 Lablicate GmbH.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ *******************************************************************************/
+package net.openchrom.thirdparty.mockito;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+
+		Activator.context = null;
+	}
+}
---
+++ openchrom/plugins/net.openchrom.thirdparty.oracle/src/net/openchrom/thirdpartylibraries/oracle/OracleDataSourceFactoryActivator.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2021 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Christoph Läubrich - initial api and implementation
+ * Philip Wenig - refactoring logger
+ *******************************************************************************/
+package net.openchrom.thirdparty.oracle;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.jdbc.DataSourceFactory;
+import org.osgi.service.log.LogService;
+import org.osgi.service.log.Logger;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class OracleDataSourceFactoryActivator implements BundleActivator {
+
+	private static final String ORACLE_JDBC_LIB_PATH = "ORACLE_JDBC_LIB_PATH";
+	private static final int INFO = 0;
+	private static final int WARNING = 1;
+	private static final int ERROR = 2;
+	private OracleDataSourceFactory oracleDataSourceFactory;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+
+		ServiceTracker<LogService, LogService> tracker = new ServiceTracker<>(bundleContext, LogService.class, null);
+		try {
+			File dataFile = getLibPath(bundleContext);
+			log(tracker, INFO, "Searching for Oracle JDBC libraries @ " + dataFile.getAbsolutePath());
+			List<URL> urls = new ArrayList<>();
+			if(dataFile.isDirectory()) {
+				File[] listFiles = dataFile.listFiles();
+				if(listFiles != null) {
+					for(File file : listFiles) {
+						if(file.getName().toLowerCase().endsWith(".jar")) {
+							urls.add(file.toURI().toURL());
+						}
+					}
+				}
+			}
+			if(urls.isEmpty()) {
+				log(tracker, WARNING, "No library files found, OracleDataSourceFactory service will not be avaiable");
+				return;
+			}
+			oracleDataSourceFactory = new OracleDataSourceFactory(new URLClassLoader(urls.toArray(new URL[0]), getClass().getClassLoader()));
+			try {
+				Driver driver = oracleDataSourceFactory.createDriver(new Properties());
+				Dictionary<String, Object> properties = new Hashtable<>();
+				properties.put(DataSourceFactory.OSGI_JDBC_DRIVER_CLASS, driver.getClass().getName());
+				properties.put(DataSourceFactory.OSGI_JDBC_DRIVER_NAME, driver.getClass().getSimpleName());
+				properties.put(DataSourceFactory.OSGI_JDBC_DRIVER_VERSION, driver.getMajorVersion() + "." + driver.getMinorVersion());
+				log(tracker, INFO, "Register OracleDataSourceFactory service with properties " + properties);
+				bundleContext.registerService(DataSourceFactory.class, oracleDataSourceFactory, properties);
+			} catch(SQLException e) {
+				log(tracker, ERROR, "Can't create driver, OracleDataSourceFactory service will not be avaiable (" + e.getCause() + ")");
+			}
+		} finally {
+			tracker.close();
+		}
+	}
+
+	private File getLibPath(BundleContext bundleContext) {
+
+		String property = System.getProperty(ORACLE_JDBC_LIB_PATH);
+		if(property != null && !property.isEmpty()) {
+			return new File(property.replace("@user.home", System.getProperty("user.home")));
+		}
+		String env = System.getenv(ORACLE_JDBC_LIB_PATH);
+		if(env != null && !env.isEmpty()) {
+			return new File(env);
+		}
+		return bundleContext.getDataFile("");
+	}
+
+	private void log(ServiceTracker<LogService, LogService> tracker, int level, String message) {
+
+		LogService logService = tracker.getService();
+		if(logService != null) {
+			Logger logger = logService.getLogger(OracleDataSourceFactoryActivator.class);
+			if(logger != null) {
+			}
+			switch(level) {
+				case WARNING:
+					logger.warn(message);
+					break;
+				case ERROR:
+					logger.error(message);
+					break;
+				default:
+					logger.info(message);
+					break;
+			}
+		} else {
+			System.out.println("[" + getClass().getSimpleName() + "] " + message);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+
+		if(oracleDataSourceFactory != null) {
+			oracleDataSourceFactory.close();
+		}
+	}
+}
