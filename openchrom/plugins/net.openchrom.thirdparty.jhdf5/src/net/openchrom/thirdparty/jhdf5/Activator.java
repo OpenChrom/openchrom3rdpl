2b8a985e22a3f0f0e9739f1502648ffb54de77f4
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-03-22 10:31:38 +0100

    Add (C) header.

--- openchrom/plugins/net.openchrom.thirdparty.jhdf5/src/net/openchrom/thirdparty/jhdf5/Activator.java
+++ openchrom/plugins/net.openchrom.thirdparty.jhdf5/src/net/openchrom/thirdparty/jhdf5/Activator.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package net.openchrom.thirdparty.jhdf5;
 
 import org.osgi.framework.BundleActivator;
@@ -16,6 +31,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 
 		Activator.context = bundleContext;
@@ -25,6 +41,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 
 		Activator.context = null;
